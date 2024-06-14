c8fe8b47c64f3ff3f6047467e2815a2e42ddaa9d
PriscilliaHilDev <priscillia.hillion@outlook.fr>
2024-06-14 12:33:25 -0400

    add icone and primevue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,29 @@
 import './bootstrap';
 import '../css/app.css';
+import 'primevue/resources/themes/saga-blue/theme.css';      // thème de PrimeVue
+import 'primevue/resources/primevue.min.css';                // CSS principal de PrimeVue
+import 'primeicons/primeicons.css';  
+
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import VueLazyload from 'vue-lazyload';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(fas)
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -15,6 +34,8 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mount(el);
     },
     progress: {
